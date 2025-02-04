62686a9e3274b92e94d6ee929fc4bdb24cc3b623
aburgy-93 <aburgy@uwalumni.com>
2025-02-04 15:12:21 -0600

    Added list of words

---
+++ wordsList.py
@@ -0,0 +1,4 @@
+words = [" python ", " hangman ", " developer ", " algorithm ", " function ", 
+         " variable ", " iteration ", " syntax ", " compile ", " exception ", 
+         " object ", " inheritance ", " dictionary ", " boolean ", " loop "]
+
